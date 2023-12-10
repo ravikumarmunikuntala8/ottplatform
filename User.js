bb1c08dddc3cbd205c6be99192fc63cf44088ad3
ravikumarmunikuntala8 <153378860+ravikumarmunikuntala8@users.noreply.github.com>
2023-12-10 12:44:34 +0530

    Update User.js

--- User.js
+++ User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   Platform: String,
   Username: String,
   Password: String,
-});
+},{ timestamps: true });
 
 const User = new mongoose.model("user", userSchema);
 
